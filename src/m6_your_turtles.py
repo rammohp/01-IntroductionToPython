51d63fdca87afe5a7d196d028408a122a93ca288
Praveen <rammohp@rose-hulman.edu>
2018-03-06 23:03:27 -0500

    .

--- src/m6_your_turtles.py
+++ src/m6_your_turtles.py
@@ -2,15 +2,15 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Praveen Rammohan.
 """
 ###############################################################################
-# TODO: 1.
+# DONE: 1.
 #   On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ###############################################################################
 
 ###############################################################################
-# TODO: 2.
+# DONE: 2.
 #   You should have RUN the  m4e_loopy_turtles  module and READ its code.
 #   (Do so now if you have not already done so.)
 #
@@ -27,3 +27,32 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #
 #   Don't forget to COMMIT-and-PUSH when you are done with this module.
 ###############################################################################
+import rosegraphics as rg
+
+window = rg.TurtleWindow()
+window.delay(10)
+
+tom = rg.SimpleTurtle()
+tom.pen = rg.Pen('red', 15)
+jerry = rg.SimpleTurtle('turtle')
+jerry.pen = rg.Pen('blue', 5)
+
+tom.forward(100)
+tom.right(90)
+tom.backward(70)
+
+jerry.draw_circle(100)
+jerry.pen_up()
+jerry.backward(400)
+jerry.pen_down()
+
+for k in range(6):
+
+    tom.draw_circle(10*k)
+    jerry.draw_regular_polygon(10-k,25+5*k)
+    jerry.pen_up()
+    jerry.forward(100)
+    jerry.pen_down()
+
+
+window.close_on_mouse_click()
